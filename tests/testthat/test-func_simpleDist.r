a836b09d3b2d5dc3fafbab8186e97a1bcca20f02
Pratik Gupte <pratikgupte16@gmail.com>
2020-07-14 00:47:26 +0200

    add test for simple dist correctness

--- tests/testthat/test-func_simpleDist.r
+++ tests/testthat/test-func_simpleDist.r
@@ -2,7 +2,9 @@ context("distance function")
 testthat::test_that("simple distance works", {
 
   # make test positions
-  testdf <- tibble::tibble(a = 1, b = 1:100)
+  testdf <- tibble::tibble(a = 1,
+                           b = 1:100,
+                           time = 1:100)
   # run function with custom col names
   testoutput <- watlastools::wat_simple_dist(testdf, x = "a", y = "b")
   # do tests
@@ -19,4 +21,25 @@ testthat::test_that("simple distance works", {
   testthat::expect_identical(testoutput, c(NA, rep(1.0, 99)),
                              info = "the distance calculation is wrong")
 
+})
+
+testthat::test_that("simple distance is correct", {
+  testdata <- data.table::fread("../testdata/whole_season_tx_435.csv")[1:1000, ]
+
+  # distance using custom fun
+  test_distances <- watlastools::wat_simple_dist(testdata,
+                                            x = "X", y = "Y",
+                                            time = "TIME")
+
+  # distance using sf
+  data_sf <- sf::st_as_sf(testdata,
+                          coords = c("X", "Y"))
+  sf::st_crs(data_sf) <- 32631
+
+  sf_distance <- sf::st_distance(data_sf$geometry[seq_len(nrow(data_sf) - 1)],
+                                 data_sf$geometry[-1], by_element = T)
+
+  sf_distance <- as.numeric(c(NA, sf_distance))
+
+  testthat::expect_equal(test_distances, sf_distance)
 })
