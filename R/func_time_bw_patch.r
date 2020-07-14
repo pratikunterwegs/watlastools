bfa6bdb1f292f0b878e545e1249a7b5882ff182c
Pratik Gupte <pratikgupte16@gmail.com>
2020-07-14 22:46:08 +0200

    add new time bw patch fun and test

--- R/func_time_bw_patch.r
+++ R/func_time_bw_patch.r
@@ -2,31 +2,35 @@
 #'
 #' @param data Dataframe containing at least the variables indicating patch
 #' start and end time.
-#' @param t1 The departure time from a hypothetical first patch.
-#' @param t2 The arrival time at a hypothetical second patch.
+#' @param t_end The departure time from a hypothetical first patch.
+#' @param t_start The arrival time at a hypothetical second patch.
 #'
 #' @return A vector of times in seconds between patches.
 #' @export
 wat_time_bw_patches <- function(data,
-                                t1 = "time_end",
-                                t2 = "time_start") {
+                                t_end = "time_end",
+                                t_start = "time_start") {
 
   # check the patches for time
   watlastools:::wat_check_data(data,
-                               names_expected = c(t1, t2))
-
-  # check there is more than one patch
-  assertthat::assert_that(nrow(data) > 1,
-                          msg = "time_bw_patches: not enough patches")
+                               names_expected = c(t_end, t_start))
 
   # order by time start
-  data.table::setorder(data, t1)
+  data.table::setorderv(data, t_start)
 
-  # get t2 - t1
-  t2 <- data[[t2]][seq_len(nrow(data) - 1)]
-  t1 <- data[[t2]][-1]
+  # handle good data case
+  if (nrow(data) > 1) {
+     # get t2 - t1
+     time1 <- data[[t_end]][seq_len(nrow(data) - 1)]
+     time2 <- data[[t_start]][-1]
 
-  time_between <- c(NA, t2 - t1)
+     time_between <- c(NA, time2 - time1)
+  } else if (nrow(data) == 1) {
+     time_between <- NA_complex_
+  } else {
+     time_between <- NA_complex_
+  }
+  
 
   assertthat::assert_that(length(time_between) == nrow(data),
   msg = "time_bw_patches: times between don't match number of patches")
