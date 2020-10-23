16057d395a896a269ac2e943954865fcb36d8165
P.R. Gupte <P284074@Workspace.rug.nl>
2020-10-23 13:57:43 +0200

    style test-func_add_tide.r with styler

--- tests/testthat/test-func_add_tide.r
+++ tests/testthat/test-func_add_tide.r
@@ -5,18 +5,24 @@ testthat::test_that("adding tide data works", {
   testdata <- data.table::fread("../testdata/whole_season_tx_435.csv")[1:1000, ]
 
   # process with rm attractor, clean data, and add tide
-  testoutput <- wat_clean_data(data = testdata,
-                               moving_window = 5,
-                               nbs_min = 3,
-                               sd_threshold = 5e5)
+  testoutput <- wat_clean_data(
+    data = testdata,
+    moving_window = 5,
+    nbs_min = 3,
+    sd_threshold = 5e5
+  )
 
-  testoutput <- wat_add_tide(data = testoutput,
-                             tide_data = "../testdata/tidesSummer2018.csv")
+  testoutput <- wat_add_tide(
+    data = testoutput,
+    tide_data = "../testdata/tidesSummer2018.csv"
+  )
 
   # do tests
   # test that the vector class is data.table and data.frame
-  testthat::expect_s3_class(object = testoutput,
-                            class = c("data.table", "data.frame"))
+  testthat::expect_s3_class(
+    object = testoutput,
+    class = c("data.table", "data.frame")
+  )
 
   # check that data are ordered in time
   testthat::expect_gt(min(as.numeric(diff(testoutput$time)), na.rm = TRUE), 0)
@@ -27,7 +33,8 @@ testthat::test_that("adding tide data works", {
   # test col names in data access
   purrr::walk(expnames, function(ename) {
     testthat::expect_true(ename %in% colnames(testoutput),
-      info = glue::glue("{ename} expected in output but not produced"))
+      info = glue::glue("{ename} expected in output but not produced")
+    )
   })
 })
 
