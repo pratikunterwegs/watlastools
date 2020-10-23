9c65a3e8b9671f638c4de88da974986ae5660a2b
P.R. Gupte <p.r.gupte@rug.nl>
2020-10-23 14:52:00 +0200

    check smoothing rev keeps coords

--- tests/testthat/test-func_cleanData.r
+++ tests/testthat/test-func_cleanData.r
@@ -22,7 +22,7 @@ testthat::test_that("cleaning raw data works", {
       length.out = 1000
     ),
     NBS = round(runif(1e3, min = 1, max = 5)),
-    TAG = "31001000435",
+    TAG = "31001000000",
     SD = 50,
     VARX = 0,
     VARY = 0,
@@ -41,6 +41,22 @@ testthat::test_that("cleaning raw data works", {
     filter_speed = TRUE,
     speed_cutoff = 150
   )
+
+  # merge test data and output
+  testdata[, time := TIME / 1000L]
+  check_coords <- data.table::merge.data.table(testdata, testoutput,
+                                               by = "time")
+
+  # test that X raw and Y raw are identical to pre-cleaning data
+  testthat::expect_identical(check_coords$X_raw, check_coords$X)
+  testthat::expect_identical(check_coords$Y_raw, check_coords$Y)
+
+  # test that cleaned x and y (median smoothed) are roughly similar to
+  # X and Y, tolerance of 0.001 in this case
+  testthat::expect_lte(max(check_coords$x - check_coords$X_raw),
+                       0.001)
+  testthat::expect_lte(max(check_coords$y - check_coords$Y_raw),
+                       0.001)
 
   # test on real data
   real_data <- data.table::fread("../testdata/whole_season_tx_435.csv")
