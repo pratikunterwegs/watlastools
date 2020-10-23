c9804ea188e6a95f28dfccdbebd6ae2c4cbc4dab
P.R. Gupte <P284074@Workspace.rug.nl>
2020-10-23 13:54:05 +0200

    style func_addTide.r with styler

--- R/func_addTide.r
+++ R/func_addTide.r
@@ -1,5 +1,5 @@
 #' Add high tide data to position data.
-#' 
+#'
 #' Also calculates the time of each position since high tide.
 #'
 #' @param data A data frame which is the output of the clean data function
@@ -17,11 +17,14 @@ wat_add_tide <- function(data,
 
   # check correct argument types and data exists
   assertthat::assert_that(is.data.frame(data),
-                          msg = "wat_add_tide: data not a dataframe")
+    msg = "wat_add_tide: data not a dataframe"
+  )
   assertthat::assert_that(is.character(tide_data),
-                          msg = "wat_add_tide: tide_data not a filename")
+    msg = "wat_add_tide: tide_data not a filename"
+  )
   assertthat::assert_that(file.exists(tide_data),
-                          msg = "wat_add_tide: tide_data not found")
+    msg = "wat_add_tide: tide_data not found"
+  )
 
   # id input is not a data.table set it so
   if (!data.table::is.data.table(data)) {
@@ -34,8 +37,9 @@ wat_add_tide <- function(data,
   names_req <- c("time", "ts")
   purrr::walk(names_req, function(fr) {
     assertthat::assert_that(fr %in% data_names,
-                            msg = glue::glue("wat_add_tide: {fr} is
-                         required but missing from data!"))
+      msg = glue::glue("wat_add_tide: {fr} is
+                         required but missing from data!")
+    )
   })
 
   # check for time in order
@@ -50,25 +54,28 @@ wat_add_tide <- function(data,
   names_req <- c("time", "tide", "tide_number")
   # read in tide data
   tide_data <- data.table::fread(tide_data)[, time :=
-                                              fasttime::fastPOSIXct(time)]
+    fasttime::fastPOSIXct(time)]
 
   purrr::walk(names_req, function(nr) {
     assertthat::assert_that(nr %in% colnames(tide_data),
-                            msg = glue::glue("wat_add_tide: {nr} is \\
-                            required but missing from tide data!"))
+      msg = glue::glue("wat_add_tide: {nr} is \\
+                            required but missing from tide data!")
+    )
   })
 
 
   # merge with tide data and order on high tide
   temp_data <- data.table::merge.data.table(data, tide_data,
-                                            by.x = "ts", by.y = "time",
-                                            all = TRUE)
+    by.x = "ts", by.y = "time",
+    all = TRUE
+  )
   data.table::setorder(temp_data, ts)
 
   # get tide_number and time since high tide
   temp_data[, tide_number := data.table::nafill(tide_number, "locf")]
   temp_data[, tidaltime := as.numeric(difftime(ts, ts[1], units = "mins")),
-            by = tide_number]
+    by = tide_number
+  ]
 
   # remove bad rows and columns
   temp_data <- temp_data[stats::complete.cases(x), ]
