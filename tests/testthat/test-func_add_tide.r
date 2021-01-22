00cb6545f0f44915a594abb272a9d5b3d5ec2198
Pratik Gupte <pratikgupte16@gmail.com>
2021-01-22 10:39:34 +0100

    correct file path

--- tests/testthat/test-func_add_tide.r
+++ tests/testthat/test-func_add_tide.r
@@ -2,7 +2,7 @@ context("add tide data\n")
 testthat::test_that("adding tide data works", {
 
   # read in data subset
-  testdata <- data.table::fread("tests/testdata/whole_season_tx_435.csv")[1:1000, ]
+  testdata <- data.table::fread("../testdata/whole_season_tx_435.csv")[1:1000, ]
 
   # process with rm attractor, clean data, and add tide
   testoutput <- wat_clean_data(
@@ -14,7 +14,7 @@ testthat::test_that("adding tide data works", {
 
   testoutput <- wat_add_tide(
     data = testoutput,
-    tide_data = "tests/testdata/tidesSummer2018.csv"
+    tide_data = "../testdata/tidesSummer2018.csv"
   )
 
   # do tests
